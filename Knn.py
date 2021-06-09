15d845124d27c00582d1455667075431a5845085
reutKeyshawn <reki8611@gmail.com>
2021-06-09 16:08:34 +0300

    in process

--- Knn.py
+++ Knn.py
@@ -23,44 +23,73 @@ def split_data(data):
     return sklearn.model_selection.train_test_split(data, data['label'], test_size=0.5)
 
 
-def runner():
-    data = read_data_from_file()
-    for i in range(101):
-
-        X_train, X_test, y_train, y_test = split_data(data)
-
-        for k in range(1, 10, 2):
-            # Create KNN Classifier
-            knn = KNeighborsClassifier(n_neighbors=k)
-            # Train the model using the training sets
-            knn.fit(X_train, y_train)
-
-            # Predict the response for test dataset
-            y_pred = knn.predict(X_test)
-
-            print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
-
-
 # calculate the Euclidean distance between two vectors wwith given p
 def euclidean_distance(row1, row2, p):
+
     distance = 0.0
     for i in range(len(row1)-1):
         distance += (row1[i] - row2[i])**p
     return distance**(1/p)
 
 
-
 # Locate the most similar neighbors
-def get_neighbors(train, test_row, num_neighbors):
+def get_neighbors(train, test_row, num_neighbors, p):
+
     distances = list()
     for train_row in train:
-        dist = euclidean_distance(test_row, train_row)
+
+        dist = euclidean_distance(test_row, train_row, p)
         distances.append((train_row, dist))
     distances.sort(key=lambda tup: tup[1])
     neighbors = list()
     for i in range(num_neighbors):
         neighbors.append(distances[i][0])
     return neighbors
+
+
+# Make a classification prediction with neighbors
+def predict_classification(train, test_row, num_neighbors, p):
+    neighbors = get_neighbors(train, test_row, num_neighbors, p)
+    output_values = [row[-1] for row in neighbors]
+    prediction = max(set(output_values), key=output_values.count)
+    return prediction
+
+
+# kNN Algorithm
+def k_nearest_neighbors(train, test, num_neighbors, p):
+    predictions = list()
+    for row in test:
+        output = predict_classification(train, row, num_neighbors, p)
+        predictions.append(output)
+    return(predictions)
+
+# Calculate accuracy percentage
+
+
+def accuracy_metric(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+ # Convert string column to float
+
+
+
+
+def runner():
+    data = read_data_from_file()
+
+    X_train, X_test, y_train, y_test = split_data(data)
+    
+    for k in range(1, 10, 2):
+
+        for p in [1, 2, float('inf')]:
+
+            predicted = k_nearest_neighbors(X_train, X_test, k, p)
+            accuracy = accuracy_metric(y_test, predicted)
+            print("Accuracy:", 1 - accuracy)
 
 
 if __name__ == "__main__":
