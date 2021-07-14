7e1f0e9e5ae765a946ee4410215f50485089db88
Rafi Dinari <rafidinary@gmail.com>
2021-07-14 10:55:28 +0300

    adding sklearn entropy for testing purposes

--- Huffman.py
+++ Huffman.py
@@ -1,3 +1,6 @@
+from scipy.stats import entropy  # for testing results only!
+
+
 def characteristics_huffman_code(probability, code):
     length_of_code = [ len(k) for k in code ]
 
@@ -70,7 +73,7 @@ def concat_huffman_bin_values(huffman_code, i, num, probability):
     return val
 
 
-def fill_proabilities(freq):
+def fill_probabilities(freq):
     freq = sorted(freq.items(), reverse=True, key=lambda x: x [ 1 ])
     probabilities: list [ float ] = sorted([ float("{:.2f}".format(frequency [ 1 ])) for frequency in freq ],
                                            reverse=True)
@@ -78,18 +81,25 @@ def fill_proabilities(freq):
 
 
 def print_encoded_first_name(results_dict):
-    first_name1 = input("please enter your first name: ")
-    print("using real statistics: ")
-    for c in first_name1:
-        print(results_dict [ c ], end="")
-    print()
+    while True:
+        try:
+            first_name1 = input("please enter your first name: ")
+            s = ''.join(map(str, [ results_dict [ c ] for c in first_name1 ]))
 
+        except KeyError:
+            print("did you enter the name in hebrew? - please try to run again")
+        else:
+            break
+    print(f"in huffman code:{s}")
+    print(entropy([ int(x) for x in s ]))
+
+
+def huffman_on_name(first_name1):
     print("using statistics based on name:")
     freq = {}
     for c in first_name1:
         freq [ c ] = freq.get(c, 0) + 1
-
-    freq, probabilities = fill_proabilities(freq)
+    freq, probabilities = fill_probabilities(freq)
     huffman_code = compute_code(probabilities)
     results_dict_name = {}
     for id, char in enumerate(freq):
@@ -102,22 +112,25 @@ def print_encoded_first_name(results_dict):
 
 
 def run():
-    freq = {'א': 0.0634, 'ב': 0.0474, 'ג': 0.013, 'ד': 0.0259, 'ה': 0.1087, 'ו': 0.1038, 'ז': 0.0133, 'ח': 0.0248,
-            'ט': 0.0124,
-            'י': 0.1106, 'כ': 0.027 + 0.0081, 'ל': 0.0739, 'מ': 0.0303 + 0.0459, 'נ': 0.011 + 0.0286, 'ס': 0.0148,
-            'ע': 0.0323,
-            'פ': 0.0027 + 0.0169, 'צ': 0.0012 + 0.0124, 'ק': 0.0214, 'ר': 0.0561, 'ש': 0.0441, 'ת': 0.0501}
-    freq, probabilities = fill_proabilities(freq)
+    hebrew_letters_freq = {'א': 0.0634, 'ב': 0.0474, 'ג': 0.013, 'ד': 0.0259, 'ה': 0.1087, 'ו': 0.1038, 'ז': 0.0133,
+                           'ח': 0.0248,
+                           'ט': 0.0124,
+                           'י': 0.1106, 'כ': 0.027 + 0.0081, 'ל': 0.0739, 'מ': 0.0303 + 0.0459, 'נ': 0.011 + 0.0286,
+                           'ס': 0.0148,
+                           'ע': 0.0323,
+                           'פ': 0.0027 + 0.0169, 'צ': 0.0012 + 0.0124, 'ק': 0.0214, 'ר': 0.0561, 'ש': 0.0441,
+                           'ת': 0.0501}
+    hebrew_letters_freq, probabilities = fill_probabilities(hebrew_letters_freq)
     huffman_code = compute_code(probabilities)
     print('אות | Huffman code ')
     print('----------------------')
     results_dict = {}
-    for id, char in enumerate(freq):
-        if huffman_code [ id ] == '':
+    for idx, char in enumerate(hebrew_letters_freq):
+        if huffman_code [ idx ] == '':
             print(" %-4r |%12s" % (char [ 0 ], 1))
             continue
-        print(' %-4r |%12s' % (char [ 0 ], huffman_code [ id ]))
-        results_dict [ char [ 0 ] ] = huffman_code [ id ]
+        print(' %-4r |%12s' % (char [ 0 ], huffman_code [ idx ]))
+        results_dict [ char [ 0 ] ] = huffman_code [ idx ]
     characteristics_huffman_code(probabilities, huffman_code)
     for _ in range(2):
         print_encoded_first_name(results_dict)
