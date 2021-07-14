758b2ed6dd6f12f7d3f30a89b8d9140dcabe3f3e
Rafi Dinari <rafidinary@gmail.com>
2021-07-14 15:19:59 +0300

    adding entropy

--- Huffman.py
+++ Huffman.py
@@ -1,4 +1,11 @@
-from scipy.stats import entropy  # for testing results only!
+import math
+
+
+def shannon_entropy(hebrew_freq, name):
+    res = 0
+    for c in name:
+        res += hebrew_freq [ c ] * math.log2(hebrew_freq [ c ])
+    return res * -1
 
 
 def characteristics_huffman_code(probability, code):
@@ -80,26 +87,28 @@ def fill_probabilities(freq):
     return freq, probabilities
 
 
-def print_encoded_first_name(results_dict):
+def print_encoded_first_name(results_dict, hebrew_letters_freq):
     while True:
         try:
             first_name1 = input("please enter your first name: ")
             s = ''.join(map(str, [ results_dict [ c ] for c in first_name1 ]))
 
         except KeyError:
-            print("did you enter the name in hebrew? - please try to run again")
+            print(
+                "did you enter the name in hebrew or are there spaces of final letters in it?\n please try to run again")
         else:
             break
     print(f"in huffman code:{s}")
-    print(entropy([ int(x) for x in s ]))
+    # huffman_on_name(first_name1)
+    print(f"shannon enteopy = {shannon_entropy(hebrew_letters_freq, first_name1)}")
 
 
 def huffman_on_name(first_name1):
     print("using statistics based on name:")
-    freq = {}
+    freq_dict = {}
     for c in first_name1:
-        freq [ c ] = freq.get(c, 0) + 1
-    freq, probabilities = fill_probabilities(freq)
+        freq_dict [ c ] = freq_dict.get(c, 0) + 1
+    freq, probabilities = fill_probabilities(freq_dict)
     huffman_code = compute_code(probabilities)
     results_dict_name = {}
     for id, char in enumerate(freq):
@@ -109,23 +118,26 @@ def huffman_on_name(first_name1):
     for c in first_name1:
         print(results_dict_name [ c ], end="")
     print()
+    print(shannon_entropy(freq_dict, first_name1))
 
 
 def run():
-    hebrew_letters_freq = {'א': 0.0634, 'ב': 0.0474, 'ג': 0.013, 'ד': 0.0259, 'ה': 0.1087, 'ו': 0.1038, 'ז': 0.0133,
-                           'ח': 0.0248,
-                           'ט': 0.0124,
-                           'י': 0.1106, 'כ': 0.027 + 0.0081, 'ל': 0.0739, 'מ': 0.0303 + 0.0459, 'נ': 0.011 + 0.0286,
-                           'ס': 0.0148,
-                           'ע': 0.0323,
-                           'פ': 0.0027 + 0.0169, 'צ': 0.0012 + 0.0124, 'ק': 0.0214, 'ר': 0.0561, 'ש': 0.0441,
-                           'ת': 0.0501}
-    hebrew_letters_freq, probabilities = fill_probabilities(hebrew_letters_freq)
+    hebrew_letters_freq_dict = {'א': 0.0634, 'ב': 0.0474, 'ג': 0.013, 'ד': 0.0259, 'ה': 0.1087, 'ו': 0.1038,
+                                'ז': 0.0133,
+                                'ח': 0.0248,
+                                'ט': 0.0124,
+                                'י': 0.1106, 'כ': 0.027 + 0.0081, 'ל': 0.0739, 'מ': 0.0303 + 0.0459,
+                                'נ': 0.011 + 0.0286,
+                                'ס': 0.0148,
+                                'ע': 0.0323,
+                                'פ': 0.0027 + 0.0169, 'צ': 0.0012 + 0.0124, 'ק': 0.0214, 'ר': 0.0561, 'ש': 0.0441,
+                                'ת': 0.0501}
+    hebrew_letters_freq_list, probabilities = fill_probabilities(hebrew_letters_freq_dict)
     huffman_code = compute_code(probabilities)
     print('אות | Huffman code ')
     print('----------------------')
     results_dict = {}
-    for idx, char in enumerate(hebrew_letters_freq):
+    for idx, char in enumerate(hebrew_letters_freq_list):
         if huffman_code [ idx ] == '':
             print(" %-4r |%12s" % (char [ 0 ], 1))
             continue
@@ -133,7 +145,8 @@ def run():
         results_dict [ char [ 0 ] ] = huffman_code [ idx ]
     characteristics_huffman_code(probabilities, huffman_code)
     for _ in range(2):
-        print_encoded_first_name(results_dict)
+        print_encoded_first_name(results_dict, hebrew_letters_freq_dict)
+    exit()
 
 
 if __name__ == "__main__":
